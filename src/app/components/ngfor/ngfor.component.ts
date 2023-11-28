9a1ec17268b319d9747167da4cded247027cc06e
Brahim Korsan El Ghabi <brahim.kor@educa.jcyl.es>
2023-11-28 19:04:51 +0100

    ngfor

---
+++ src/app/components/ngfor/ngfor.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-ngfor',
+  templateUrl: './ngfor.component.html',
+  styleUrls: ['./ngfor.component.css']
+})
+export class NgforComponent {
+  users = [{id: 100, firstName: "Ramesh", lastName: "Fadatare", emailId: "ramesh@gmail.com"}, {
+    id: 101,
+    firstName: "Tony",
+    lastName: "Stark",
+    emailId: "tony@gmail.com"
+  }, {id: 102, firstName: "Tom", lastName: "Cruise", emailId: "tom@gmail.com"}]
+}
