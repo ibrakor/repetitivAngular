0a35496ef5194755bc83cfe42d3b1b70f701073c
Brahim Korsan El Ghabi <brahim.kor@educa.jcyl.es>
2023-11-28 19:05:46 +0100

    ngfor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {NgifComponent} from "./components/ngif/ngif.component";
 import { NgswitchComponent } from './components/ngswitch/ngswitch.component';
+import {NgforComponent} from "./components/ngfor/ngfor.component";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NgifComponent,
-    NgswitchComponent
+    NgswitchComponent,
+    NgforComponent
   ],
   imports: [
     BrowserModule
